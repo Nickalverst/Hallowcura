78a469bbdb1631fb80c17853d06543f6a9a1ee9d
Nicolas Sousa <nicolasbarbierisousa@icloud.com>
2022-06-04 12:28:11 -0300

    Êxito na detecção de colisões.

--- Jogo1/Gerenciador_Colisao.cpp
+++ Jogo1/Gerenciador_Colisao.cpp
@@ -13,20 +13,24 @@ void Gerenciador_Colisao::Colisao()
 {
     for (vector<Inimigo*>::iterator it = LIs.begin(); it != LIs.end(); it++)
     {
-        if (abs((*it)->getPosicao().x - j1->getPosicao().x) <= ((*it)->getTamanho().x / 2 + j1->getTamanho().x / 2) /*&&
-            abs((*it)->getPosicao().y - j1->getPosicao().y) <= ((*it)->getTamanho().y + j1->getTamanho().y)*/)
+        float difPosX = (*it)->getPosicao().x - j1->getPosicao().x;
+        float difPosY = (*it)->getPosicao().y - j1->getPosicao().y;
+        if (abs(difPosX) <= SIZE*((*it)->getTamanho().x / 2 + j1->getTamanho().x / 2) &&
+            abs(difPosY) <= SIZE*((*it)->getTamanho().y / 2 + j1->getTamanho().y / 2))
         {
-            cout << "Diferença de posicoes: " << abs((*it)->getPosicao().x - j1->getPosicao().x) <<
-                "\nTamanho do sprite: " << (*it)->getTamanho().x << endl;
             cout << "Bateu! " << endl;
-            
-            if (((*it)->getPosicao().x - (*it)->getTamanho().x / 2) <= (j1->getPosicao().x + j1->getTamanho().x / 2) &&
-                ((*it)->getPosicao().x + (*it)->getTamanho().x / 2) >= ((j1->getPosicao().x) - j1->getTamanho().x / 2) &&
-                ((*it)->getPosicao().y - (*it)->getTamanho().y / 2) <= (j1->getPosicao().y + j1->getTamanho().y / 2) &&
-                ((*it)->getPosicao().y + (*it)->getTamanho().y / 2) >= (j1->getPosicao().y - j1->getTamanho().y / 2))
-            {
-                cout << "Bateu! 2" << endl;
-            }
+            //(*it)->atacar();
+            // j1->tomarDano(difPosX); tirar vida e dar stagger
+            /* Dentro da função tomar dano :
+                if (difPosX > 0)
+                {
+                    cout << "Bateu pela esquerda. " << endl;
+                }
+                else
+                {
+                    cout << "Bateu pela direita. " << endl;
+                }
+            */
         }
     }
 
